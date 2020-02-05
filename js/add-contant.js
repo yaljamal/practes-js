87e01fce08f82600958afa13de541a61eef48c97
yaljamal <yazan.aljamal1@gmail.com>
2020-02-05 12:07:40 +0200

    add some edit

--- js/add-contant.js
+++ js/add-contant.js
@@ -1,14 +1,25 @@
-var today = new Date();
- var hourNow = today.getHours();
-  var greeting; 
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting; 
  
-if (hourNow > 18) { 
+
+function creategreeting(hourNow)
+{
+        console.log(hourNow);
+        var greeting = 'Something went wrong!';  
+if (hourNow >= 18 && hourNow <23)
+{
         greeting = 'Good evening, Class!';
-       } else if (hourNow > 12) { 
-               greeting = 'Good afternoon, Class!';
-               } else if (hourNow >= 0) { 
-                greeting = 'Good morning!'; 
-                } else {  
-                greeting = 'Something went wrong!';   
-                } 
-        document.write('<h3>'+greeting+'</h3>');
+       }
+        else if (hourNow >= 12 && hourNow > 18) 
+       {
+        greeting = 'Good afternoon, Class!';
+        }
+        else if (hourNow >= 0 && hourNow < 12) 
+        {
+        greeting = 'Good morning!'; 
+        } 
+       return greeting
+} 
+var hourNow2 = prompt(" what the time now ?");
+ document.write('<h3>'+creategreeting(hourNow2)+'</h3>');
